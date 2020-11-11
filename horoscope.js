1a35fe911897b6572b24a2877d3d2ca6d8a5f61c
Joseph Kelly <jkelly2191@gmail.com>
2020-11-10 21:33:33 -0600

    added zodiac signs to birthdate ranges

--- horoscope.js
+++ horoscope.js
@@ -1,10 +1,36 @@
 // var sign = "";
-$(document).ready(function(){
+$(function(){
+
+  var sign = ''
+
   $("#birthdayRangeSelector").on("change", function(e) {
-    console.log($(this).val());
+    // console.log($(this).val());
+    sign = $(this).val()
+    $(this.val).text(sign)
+    console.log(sign)
+
+
+
+
+
+    $.ajax({
+
+      type:'POST',
+      url:'https://aztro.sameerkumar.website?sign=' + sign + '&day=today',
+      success:function(data){
+        console.log(data);
+
+      }
+
+    });
+
   })
 
-  // var dateSignMap = {
+
+})
+
+
+
   //   "Sep 22 - Nov 21": "leo",
   //   "Nov 22 - Dec 21": "pisces",
   //   "Aug 23 - Sep 22": "aries",
@@ -12,38 +38,20 @@ $(document).ready(function(){
   // };
 
 
-
-
-
-  var sign = 'pisces' 
-
-  // 'aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'sagittarius', 'capricorn', 'aquarius', 'pisces'
-
-  // Uses API to get horoscope
-  // for (var i = 0; i < signArry.length; i++) {
-
-    $.ajax({
-
-      type:'POST',
-      url:'https://aztro.sameerkumar.website?sign=' + sign + '&day=today',
-      success:function(data){
-      console.log(data);
-
-      }
-
-   });
-
+  // var zodiacDates = {
+  //   aries:'Mar 21 - Apr 20',
+  //   taurus:'Apr 21 - May 20',
+  //   gemini:'May 21 - Jun 21',
+  //   cancer:'Jun 22 - Jul 22',
+  //   leo:'Jul 23 - Aug 22',
+  //   virgo:'Aug 23 - Sep 22',
+  //   libra:'Sep 23 - Oct 22',
+  //   scorpio:'Oct 23 - Nov 22',
+  //   sagittarius:'Nov 23 - Dec 21',
+  //   capricorn:'Dec 22 - Jan 19',
+  //   aquarius:'Jan 20 - Feb 18',
+  //   pisces:'Feb 19 - Mar 20',
   // }
-
-
-})
-
-// when user inputs their birthday match to range of signs birth date and retrieve sign name
-  //
-// {
-
-// }
-
 
 
 
