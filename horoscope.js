4312138e57c70ed48a1700dfdb7488eb4a93b565
Joseph Kelly <jkelly2191@gmail.com>
2020-11-12 19:44:36 -0600

    console.log Sign, Reading and compatibility

--- horoscope.js
+++ horoscope.js
@@ -4,27 +4,35 @@ $(function(){
   var sign = ''
 
   $("#birthdayRangeSelector").on("change", function(e) {
-    // console.log($(this).val());
+
+
     sign = $(this).val()
     $(this.val).text(sign)
-    console.log(sign)
-
-
-
-
 
     $.ajax({
 
       type:'POST',
       url:'https://aztro.sameerkumar.website?sign=' + sign + '&day=today',
       success:function(data){
+
+        var zodiac = $("#zodiac")
+        var userSign = ("Sign: " + sign)
+        var reading = ("Zodiac Reading: " + data.description)
+        var compatibility = ("Compatibility: " + data.compatibility)
+
+        console.log(userSign)
+        console.log(reading)
+        console.log(compatibility)
         console.log(data);
+
+        
 
       }
 
+
     });
 
-  })
+    })
 
 
 })
